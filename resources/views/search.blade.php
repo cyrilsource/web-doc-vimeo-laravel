b161085d2342af6c7b5f37a28c27ba9c4c4e0b0f
cyril <cyrilbron78@gmail.com>
2021-06-24 21:50:14 +0200

    search: for one video result you have a medium size card

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -7,13 +7,25 @@
 @include('header')
     <div class="entry-content-themes">
         <div class="entry-content-themes__grid">
-            @if(count($videos) > 0)
+            @if(count($videos) > 1)
                 @foreach ($videos as $video)
                     <a  href="{{ url('/video') }}/{{ $video->slug }}/{{ $video->id }}" class="entry-content-themes__card transition-fade">
                         <img src="{{ $video->thumbnail_large }}" alt="{{ $video->title }}">
                         <h2 class="entry-content-themes__title">{{ $video->title }}</h2>
                     </a>
                 @endforeach
+         </div>
+            @elseif (count($videos) == 1)
+                <div class="horizontal-align">
+                    <div class="entry-content-themes__single">
+                        @foreach ($videos as $video)
+                            <a  href="{{ url('/video') }}/{{ $video->slug }}/{{ $video->id }}" class="entry-content-themes__card transition-fade">
+                                <img src="{{ $video->thumbnail_large }}" alt="{{ $video->title }}">
+                                <h2 class="entry-content-themes__title">{{ $video->title }}</h2>
+                            </a>
+                        @endforeach
+                    </div>
+                </div>
             @else
             <section class="center">
                 <div class="not-found">
@@ -21,8 +33,6 @@
                 </div>
             </section>
             @endif
-
-        </div>
 
     </div><!-- .entry-content-themes-->
 </main>
